c105b5fad96a80ff908d9267e1478ccf6a81b427
changrita1114 <hardheard101@gmail.com>
2020-10-20 16:22:55 -0700

    add a revised version

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,5 @@
+# Credit: Ryan Boris 
+
 # import modules
 import os
 import csv
@@ -5,57 +7,49 @@ import csv
 # file reading location
 csvpath = os.path.join("Resources", "budget_data.csv")
 
+dates = []
+profits_losses = []
+
 # reading file using CSV module
 with open(csvpath) as csvfile:
-    
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter = ",")
-    
     # Read the header row first
     csv_header = next(csvreader)
     
-    # Count total months
-    # It worked, but I don't even know why. What does "1" mean ?)
-    total_month = sum(1 for row in csvreader)
-    
-    # Count profit total
-    # I was thinking to insert a for loop from this line but failed. Something like
-    #total_profit=[]
-    #for row in csvreader:
-    #total_profit.append(int(row(1)))
-    
-    # Count average change
-    # Dunno how to do
+    # Making data into the dates and profits_losses lists
+    for row in (csvreader):
+        dates.append(row[0])
+        profits_losses.append(int(row[1]))
 
-    # look uo the greatest increase in profits
-    # Dunno how to do
-    
-    # look up the greatest decrease in profits
-    # Dunno how to do
+# # Count total mounths
+# print(len(dates))
+# # Count total amounts
+# print(sum(profits_losses))
+# Count average change
+cumulatives =[]
+for index, item in enumerate(profits_losses):
+    try:
+        price =  profits_losses[index]
+        next_price = profits_losses[index + 1]
+        cumulatives.append(next_price - price)
+    except IndexError:
+        pass
 
-    # print in terminal
-    print("Financial Analysis")
-    print("---------------------------------")
-    print("Total Months: " + str(total_month))
-    #print("Total: $" + str(sum(total_profit))
-    #print("Average Change: $" + str(average_change))
-    #print("Greatest Increase in Profits: ")
-    #print("Greatest Decrease in Profits: ")
+average_change = round(sum(cumulatives)/len(cumulatives),2)
 
-#Zip lists together
-#cleaned_csv = zip(total_month, total_profit, average_change, Greatest Increase in Profits,
-#             Greatest Decrease in Profits)
+# look uo the greatest increase and decrease in profits
+max_increase = max(cumulatives)
+min_decrease = min(cumulatives)
 
-# File output location
-# output_file = os.path.join("analysis", "results.txt")
+max_index = cumulatives.index(max_increase)
+min_index = cumulatives.index(min_decrease)
 
-# Open the output file
-# with open(output_file, "w") as datafile:
-       #writer = csv.writer(datafile)
-
-# Write the header row
-    #writer.writerow(["total_month", "total_profit", "average_change", "Greatest Increase in Profits",
-                     # "Greatest Decrease in Profits"])
-
- # Write in zipped rows
-    # writer.writerows(cleaned_csv)
+# print in terminal
+print("Financial Analysis")
+print("---------------------------------")
+print("Total Months: " + str(len(dates)))
+print("Total: $" + str(sum(profits_losses)))
+print("Average Change: $" + str(average_change))
+print("Greatest Increase in Profits: " + dates[max_index +1] + " $" + str(max_increase))
+print("Greatest Decrease in Profits: " + dates[min_index +1] + " $" + str(min_decrease))
